0dfc98defd79ae88f7029fcda3728ff6a4db7ecc
o.mannion <o.mannion@7487ce81-0fee-146b-97b0-a58def4d1908>
2011-01-04 23:50:07 +0000

    git-svn-id: https://readmytables.googlecode.com/svn/trunk@12 7487ce81-0fee-146b-97b0-a58def4d1908

---
+++ readmytables/src/org/omancode/rmt/cellreader/narrow/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Narrowing and type checking functions.
+ *
+ * @author Oliver Mannion
+ *
+ */
+package org.omancode.rmt.cellreader.narrow;
--- readmytables/src/org/omancode/rmt/tablereader/file/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Table reading from files.
- *
- * @author Oliver Mannion
- *
- */
-package org.omancode.rmt.tablereader.file;
